19020bb06b47ca98c6f49705988d1cddb6d1233b
Asphaltian <amirulhh2009@gmail.com>
2023-12-10 20:38:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,21 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <title>Softsplit Studios</title>
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://softsplit.org" />
-  <meta property="og:title" content="Softsplit Studios" />
-  <meta
-    property="og:description"
-    content="We make stuff in our spare time"
-  />
   <meta property="og:image" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
+  <meta property="og:image:alt" content="We make stuff in our spare time" />
+  <meta property="og:image:width" content="512" />
+  <meta property="og:image:height" content="512" />
+  <meta property="og:site_name" content="Softsplit Studios" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Softsplit Studios" />
+  <meta property="og:url" content="https://softsplit.org" />
+  <meta property="og:description" content="We make stuff in our spare time" />
+
+  <meta name="twitter:image:src" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
+  <meta name="twitter:site" content="@Softsplit" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Softsplit Studios" />
+  <meta name="twitter:description" content="We make stuff in our spare time" />
   <RouterView />
 </template>
 
