d4cc0bea00dab1deaf038cf7eebb9b98e199799f
Asphaltian <amirulhh2009@gmail.com>
2023-12-10 20:17:50 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://softsplit.org" />
+  <meta property="og:title" content="Softsplit Studios" />
+  <meta
+    property="og:description"
+    content="We make stuff in our spare time"
+  />
+  <meta property="og:image" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
   <RouterView />
 </template>
 
