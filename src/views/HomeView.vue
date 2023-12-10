737f8c8710feea699539059a56f51b45cebc4c88
Asphaltian <amirulhh2009@gmail.com>
2023-12-10 22:05:59 +0800

    Fix meta

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,31 @@
 <template>
-  <div>
-    <NavigationBar
-      :isMobile="isMobile"
-      :showMobileMenu="showMobileMenu"
-      @toggleMobileMenu="toggleMobileMenu"
-    />
+  <Head>
+    <meta property="og:image" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
+    <meta property="og:image:alt" content="We make stuff in our spare time" />
+    <meta property="og:image:width" content="512" />
+    <meta property="og:image:height" content="512" />
+    <meta property="og:site_name" content="Softsplit Studios" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://softsplit.org" />
+    <meta property="og:description" content="We make stuff in our spare time" />
 
-    <div v-if="!isMobile || (isMobile && !showMobileMenu)">
-      <HomeIntro v-show="showHomeIntro" />
-      <HomeGames v-show="showHomeGames" />
-      <HomeAboutUs v-show="showHomeAboutUs" />
-      <NavigationFooter v-show="showNavigationFooter" />
-    </div>
+    <meta name="twitter:image:src" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
+    <meta name="twitter:site" content="@Softsplit" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Softsplit Studios" />
+    <meta name="twitter:description" content="We make stuff in our spare time" />
+  </Head>
+  <NavigationBar
+    :isMobile="isMobile"
+    :showMobileMenu="showMobileMenu"
+    @toggleMobileMenu="toggleMobileMenu"
+  />
+
+  <div v-if="!isMobile || (isMobile && !showMobileMenu)">
+    <HomeIntro v-show="showHomeIntro" />
+    <HomeGames v-show="showHomeGames" />
+    <HomeAboutUs v-show="showHomeAboutUs" />
+    <NavigationFooter v-show="showNavigationFooter" />
   </div>
 </template>
 
@@ -22,31 +36,7 @@ import HomeIntro from '@/components/HomeIntro.vue'
 import HomeGames from '@/components/HomeGames.vue'
 import HomeAboutUs from '@/components/HomeAboutUs.vue'
 import { ref, onMounted, onBeforeUnmount } from 'vue'
-import { useHead } from '@unhead/vue'
-
-const pageTitle = 'Softsplit Studios';
-const siteUrl = 'https://softsplit.org';
-const siteDescription = 'We make stuff in our spare time';
-const imageUrl = 'https://softsplit.org/assets/logo2-SHwmP7UB.png';
-
-useHead({
-  title: pageTitle,
-  meta: [
-    { property: 'og:image', content: imageUrl },
-    { property: 'og:image:alt', content: 'We make stuff in our spare time' },
-    { property: 'og:image:width', content: '512' },
-    { property: 'og:image:height', content: '512' },
-    { property: 'og:site_name', content: 'Softsplit Studios' },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: siteUrl },
-    { property: 'og:description', content: siteDescription },
-    { name: 'twitter:image:src', content: imageUrl },
-    { name: 'twitter:site', content: '@Softsplit' },
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'Softsplit Studios' },
-    { name: 'twitter:description', content: siteDescription },
-  ],
-});
+import { Head } from '@unhead/vue/components'
 
 const isMobile = ref(false)
 const showMobileMenu = ref(false)
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -5,31 +5,7 @@ import TeamOutro from '@/components/TeamOutro.vue'
 import NavigationFooter from '@/components/NavigationFooter.vue'
 import NavigationBar from '@/components/NavigationBar.vue'
 import { ref, onMounted, onBeforeUnmount } from 'vue'
-import { useHead } from '@unhead/vue'
-
-const pageTitle = 'Team — Softsplit Studios';
-const siteUrl = 'https://softsplit.org';
-const siteDescription = 'We make stuff in our spare time';
-const imageUrl = 'https://softsplit.org/assets/logo2-SHwmP7UB.png';
-
-useHead({
-  title: pageTitle,
-  meta: [
-    { property: 'og:image', content: imageUrl },
-    { property: 'og:image:alt', content: 'We make stuff in our spare time' },
-    { property: 'og:image:width', content: '512' },
-    { property: 'og:image:height', content: '512' },
-    { property: 'og:site_name', content: 'Softsplit Studios' },
-    { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: siteUrl },
-    { property: 'og:description', content: siteDescription },
-    { name: 'twitter:image:src', content: imageUrl },
-    { name: 'twitter:site', content: '@Softsplit' },
-    { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'Softsplit Studios' },
-    { name: 'twitter:description', content: siteDescription },
-  ],
-});
+import { Head } from '@unhead/vue/components'
 
 const isMobile = ref(false)
 const showMobileMenu = ref(false)
@@ -53,18 +29,34 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div>
-    <NavigationBar
-      :isMobile="isMobile"
-      :showMobileMenu="showMobileMenu"
-      @toggleMobileMenu="toggleMobileMenu"
-    />
+  <Head>
+    <title>Team — Softsplit Studios</title>
+    <meta property="og:image" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
+    <meta property="og:image:alt" content="We make stuff in our spare time" />
+    <meta property="og:image:width" content="512" />
+    <meta property="og:image:height" content="512" />
+    <meta property="og:site_name" content="Softsplit Studios" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://softsplit.org" />
+    <meta property="og:description" content="We make stuff in our spare time" />
 
-    <div v-if="!isMobile || (isMobile && !showMobileMenu)">
-      <TeamIntro />
-      <TeamList />
-      <TeamOutro />
-      <NavigationFooter />
-    </div>
+    <meta name="twitter:image:src" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
+    <meta name="twitter:site" content="@Softsplit" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Softsplit Studios" />
+    <meta name="twitter:description" content="We make stuff in our spare time" />
+  </Head>
+  
+  <NavigationBar
+    :isMobile="isMobile"
+    :showMobileMenu="showMobileMenu"
+    @toggleMobileMenu="toggleMobileMenu"
+  />
+
+  <div v-if="!isMobile || (isMobile && !showMobileMenu)">
+    <TeamIntro />
+    <TeamList />
+    <TeamOutro />
+    <NavigationFooter />
   </div>
 </template>
