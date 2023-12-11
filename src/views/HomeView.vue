0497440cd1c717f1f981935541167f7c774b2dbb
Asphaltian <amirulhh2009@gmail.com>
2023-12-11 21:08:37 +0800

    Test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,4 @@
 <template>
-  <Head>
-    <meta property="og:image" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
-    <meta property="og:image:alt" content="We make stuff in our spare time" />
-    <meta property="og:image:width" content="512" />
-    <meta property="og:image:height" content="512" />
-    <meta property="og:site_name" content="Softsplit Studios" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://softsplit.org" />
-    <meta property="og:description" content="We make stuff in our spare time" />
-
-    <meta name="twitter:image:src" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
-    <meta name="twitter:site" content="@Softsplit" />
-    <meta name="twitter:title" content="Softsplit Studios" />
-    <meta name="twitter:description" content="We make stuff in our spare time" />
-
-    <meta content="#FFA500" data-react-helmet="true" name="theme-color" />
-  </Head>
   <NavigationBar
     :isMobile="isMobile"
     :showMobileMenu="showMobileMenu"
@@ -37,7 +20,6 @@ import HomeIntro from '@/components/HomeIntro.vue'
 import HomeGames from '@/components/HomeGames.vue'
 import HomeAboutUs from '@/components/HomeAboutUs.vue'
 import { ref, onMounted, onBeforeUnmount } from 'vue'
-import { Head } from '@unhead/vue/components'
 
 const isMobile = ref(false)
 const showMobileMenu = ref(false)
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -5,7 +5,6 @@ import TeamOutro from '@/components/TeamOutro.vue'
 import NavigationFooter from '@/components/NavigationFooter.vue'
 import NavigationBar from '@/components/NavigationBar.vue'
 import { ref, onMounted, onBeforeUnmount } from 'vue'
-import { Head } from '@unhead/vue/components'
 
 const isMobile = ref(false)
 const showMobileMenu = ref(false)
@@ -29,25 +28,6 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <Head>
-    <title>Team — Softsplit Studios</title>
-    <meta property="og:image" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
-    <meta property="og:image:alt" content="We make stuff in our spare time" />
-    <meta property="og:image:width" content="512" />
-    <meta property="og:image:height" content="512" />
-    <meta property="og:site_name" content="Softsplit Studios" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://softsplit.org" />
-    <meta property="og:description" content="We make stuff in our spare time" />
-
-    <meta name="twitter:image:src" content="https://softsplit.org/assets/logo2-SHwmP7UB.png" />
-    <meta name="twitter:site" content="@Softsplit" />
-    <meta name="twitter:title" content="Team — Softsplit Studios" />
-    <meta name="twitter:description" content="We make stuff in our spare time" />
-
-    <meta content="#FFA500" data-react-helmet="true" name="theme-color" />
-  </Head>
-  
   <NavigationBar
     :isMobile="isMobile"
     :showMobileMenu="showMobileMenu"
