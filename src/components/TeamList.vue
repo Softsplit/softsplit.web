f471f7c9a9100ead190cf24328e285cdc18979a8
Asphaltian <amirulhh2009@gmail.com>
2024-02-20 10:38:58 +0800

    Update TeamList.vue

--- src/components/TeamList.vue
+++ src/components/TeamList.vue
@@ -4,7 +4,7 @@
       <div class="member-container">
         <img alt="Member picture" src="@/assets/asphaltian.jpg" class="member-img" />
         <div class="member-name">Asphaltian</div>
-        <div class="member-title">Founder/Programmer</div>
+        <div class="member-title">Founder, Programmer</div>
       </div>
       <div class="member-container">
         <img alt="Member picture" src="@/assets/badandbest.webp" class="member-img" />
