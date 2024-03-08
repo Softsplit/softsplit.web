10ceb67ef5909c2d082cc49cbcae0e2aed1f0aa0
Asphaltian <amirulhh2009@gmail.com>
2024-03-08 18:23:52 +0800

    Update members

--- src/components/TeamList.vue
+++ src/components/TeamList.vue
@@ -7,8 +7,8 @@
         <div class="member-title">Founder, Programmer</div>
       </div>
       <div class="member-container">
-        <img alt="Member picture" src="@/assets/badandbest.webp" class="member-img" />
-        <div class="member-name">badandbest</div>
+        <img alt="Member picture" src="@/assets/bubbl.webp" class="member-img" />
+        <div class="member-name">bub.bl</div>
         <div class="member-title">Programmer</div>
       </div>
       <div class="member-container">
@@ -24,7 +24,7 @@
       <div class="member-container">
         <img alt="Member picture" src="@/assets/amystudios.png" class="member-img" />
         <div class="member-name">AmyStudios</div>
-        <div class="member-title">Texture Artist</div>
+        <div class="member-title">3D Artist</div>
       </div>
     </div>
   </div>
