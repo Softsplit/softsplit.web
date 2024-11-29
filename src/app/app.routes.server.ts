36f8032638decafc6c36b6604fe5389bd51a56c8
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2024-11-30 02:54:22 +0800

    Erm

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,14 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'team',
-    renderMode: RenderMode.Prerender
-  },
-  {
     path: '**',
     renderMode: RenderMode.Server
   }
